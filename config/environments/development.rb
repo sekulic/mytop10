2b6204c6c0877a2ad7e30ce454ee5e41048204ec
sekulic <sekulic.radosav@yahoo.com>
2014-08-27 15:10:48 +0200

    Fixing index3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  ENV['AWS_ACCESS_KEY_ID'] = 'AKIAIGWANRR2ZR6TYVFQ';
-  ENV['AWS_SECRET_ACCESS_KEY'] = 'IYf531mGM+cR6hURqAdZF1HeKL1BTgqBATcaNmuZ';
-  ENV['S3_BUCKET_NAME'] = 'mytopten';
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
